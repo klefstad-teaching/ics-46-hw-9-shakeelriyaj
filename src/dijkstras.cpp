77f031ca6a15b846099ce85cf85b4caa69ff6c19
shakeelriyaj <shakeel.riyaj@gmail.com>
2025-03-12 21:33:50 +0000

    path'

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -62,10 +62,10 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
-    cout << "Path: ";
-    for (size_t i = 0; i < path.size(); i++) {
-        cout << path[i];
-        if (i + 1 < path.size()) {
+    cout << "V: ";
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << v[i];
+        if (i + 1 < v.size()) {
             cout << " -> ";
         }
     }
