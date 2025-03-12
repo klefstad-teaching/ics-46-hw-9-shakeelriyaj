516d2f760606a78690226f540120ae6a62eb58fb
shakeelriyaj <shakeel.riyaj@gmail.com>
2025-03-12 21:35:10 +0000

    printied errors:

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -109,8 +109,7 @@ void print_word_ladder(const vector<string>& ladder) {
         return;
     }
     for (size_t i = 0; i < ladder.size(); i++) {
-        if (i > 0) cout << " -> ";
-        cout << ladder[i];
+        cout << ladder[i] << " ";
     }
     cout << endl;
 }
